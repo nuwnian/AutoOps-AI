65f4369cc528883c695a9b0478e466412db92b6b
NuwaNian <nuwa.ninian@gmail.com>
2025-08-15 23:29:20 +0700

    Show current time and add /health endpoint

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask
+from datetime import datetime
+
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def home():
-    return "Hello from Mini Azure App!"
+    now = datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S UTC')
+    return f"Hello from Mini Azure App!<br>Current server time: {now}"
+@app.route("/health")
+def health():
+    return "ok"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
